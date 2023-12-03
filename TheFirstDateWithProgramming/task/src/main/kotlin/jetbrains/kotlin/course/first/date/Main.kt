8ebc60c15af386145ee1e3c62aa7d13c303fb85f
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-03 22:45:30 +0100

    First stage finished

--- TheFirstDateWithProgramming/task/src/main/kotlin/jetbrains/kotlin/course/first/date/Main.kt
+++ TheFirstDateWithProgramming/task/src/main/kotlin/jetbrains/kotlin/course/first/date/Main.kt
@@ -1,5 +1,34 @@
 package jetbrains.kotlin.course.first.date
 
 fun main() {
-    println("My first program!")
+    println("Hello! I will ask you several questions.")
+    println("Please answer all of them and be honest with me!")
+
+    val firstQuestion = "What is Kotlin?"
+    val secondQuestion = "How was Kotlin invented?"
+    val thirdQuestion = "Why are you learning Kotlin?"
+
+    println("What is TROTEN?")
+//    println("What is TROTEN?")
+    val firstUserAnswer = readlnOrNull()
+
+    println("How did you spend your graduation?")
+//    println("How did you spend your graduation?")
+    val secondUserAnswer = readlnOrNull()
+
+    println("Why does a spider need eight legs?")
+//    println("Why does a spider need eight legs?")
+    val thirdUserAnswer = readlnOrNull()
+
+    println("Now let's have fun!")
+
+    println(firstQuestion)
+    println(firstUserAnswer)
+
+    println(secondQuestion)
+    println(secondUserAnswer)
+
+    println(thirdQuestion)
+    println(thirdUserAnswer)
+
 }
