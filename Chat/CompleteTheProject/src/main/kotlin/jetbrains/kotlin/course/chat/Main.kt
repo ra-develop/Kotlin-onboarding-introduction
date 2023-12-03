f20dc292ff3ce45d0fc9356f0371e67db52f5ec0
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-04 00:00:41 +0100

    Stage: "Warm up": Task 3 finished.

--- Chat/CompleteTheProject/src/main/kotlin/jetbrains/kotlin/course/chat/Main.kt
+++ Chat/CompleteTheProject/src/main/kotlin/jetbrains/kotlin/course/chat/Main.kt
@@ -1,5 +1,10 @@
 package jetbrains.kotlin.course.chat
 
 fun main() {
-    // Write your solution here
+    println("Hello! I'm glad to meet you, let me get to know you better! What is your name?")
+    val firstAnswer = readlnOrNull()
+    println("Nice to meet you, $firstAnswer! My name is Kotlin Bot! I am a young programming language created in 2010. How old are you?")
+    val secondAnswer = readlnOrNull()
+    println("$secondAnswer is great! I hope you successfully complete this course! Anyone can learn programming at any age!")
+
 }
--- WarmUp/task/src/main/kotlin/jetbrains/kotlin/course/warmup/Main.kt
+++ WarmUp/task/src/main/kotlin/jetbrains/kotlin/course/warmup/Main.kt
@@ -1,5 +1,33 @@
 package jetbrains.kotlin.course.warmup
 
+
+fun getGameRules(wordLength: Int, maxAttemptsCount: Int, secretExample: String): String {
+    return "Welcome to the game! \n" +
+            "\n" +
+            "Two people play this game: one chooses a word (a sequence of letters), the other guesses it. In this version, the computer chooses the word: a sequence of $wordLength letters (for example, $secretExample). The user has several attempts to guess it (the max number is $maxAttemptsCount). For each attempt, the number of complete matches (letter and position) and partial matches (letter only) is reported. \n" +
+            "\n" +
+            "For example, with $secretExample as the hidden word, the BCDF guess will give 1 full match (C) and 1 partial match (B)."
+
+}
+
+fun generateSecret(): String {
+    return "ABCD"
+}
+
+fun countPartialMatches(secret: String, guess: String): Int = TODO("Not implemented yet")
+
+fun countExactMatches(secret: String, guess: String): Int = TODO("Not implemented yet")
+
 fun main() {
-    // Write your solution here
+    val wordLength = 4
+    val maxAttemptsCount = 3
+    val secretExample = "ACEB"
+
+    println("Welcome to the game! \n" +
+            "\n" +
+            "Two people play this game: one chooses a word (a sequence of letters), the other guesses it. In this version, the computer chooses the word: a sequence of $wordLength letters (for example, $secretExample). The user has several attempts to guess it (the max number is $maxAttemptsCount). For each attempt, the number of complete matches (letter and position) and partial matches (letter only) is reported. \n" +
+            "\n" +
+            "For example, with $secretExample as the hidden word, the BCDF guess will give 1 full match (C) and 1 partial match (B).")
+
+
 }
