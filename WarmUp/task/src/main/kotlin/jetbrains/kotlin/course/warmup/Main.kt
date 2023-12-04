15291e95a2bd4abf0020626a119f98f31eab701e
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-04 10:45:14 +0100

    Stage: "Warm up": Task 4 finished.

--- WarmUp/task/src/main/kotlin/jetbrains/kotlin/course/warmup/Main.kt
+++ WarmUp/task/src/main/kotlin/jetbrains/kotlin/course/warmup/Main.kt
@@ -18,16 +18,29 @@ fun countPartialMatches(secret: String, guess: String): Int = TODO("Not implemen
 
 fun countExactMatches(secret: String, guess: String): Int = TODO("Not implemented yet")
 
+fun isComplete(secret: String, guess: String): Boolean = true
+
+fun playGame(secret: String, wordLength: Int, maxAttemptsCount: Int) {
+    var complete = false
+    var attempts = 0
+    while(!complete /*&& attempts < maxAttemptsCount*/) {
+        val guess = safeReadLine()
+//        attempts++
+        complete = isComplete(secret, guess)
+    }
+}
+
+
 fun main() {
     val wordLength = 4
     val maxAttemptsCount = 3
     val secretExample = "ACEB"
 
-    println("Welcome to the game! \n" +
-            "\n" +
-            "Two people play this game: one chooses a word (a sequence of letters), the other guesses it. In this version, the computer chooses the word: a sequence of $wordLength letters (for example, $secretExample). The user has several attempts to guess it (the max number is $maxAttemptsCount). For each attempt, the number of complete matches (letter and position) and partial matches (letter only) is reported. \n" +
-            "\n" +
-            "For example, with $secretExample as the hidden word, the BCDF guess will give 1 full match (C) and 1 partial match (B).")
+    println(getGameRules(wordLength, maxAttemptsCount, secretExample))
 
+    val secret = generateSecret()
 
+    println("Please input your guess. It should be of length $wordLength.")
+
+    playGame(secret, wordLength, maxAttemptsCount)
 }
