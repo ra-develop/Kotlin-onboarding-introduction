0bb4d73f5e1b772b29c0a8a9fafb0856ca7c4321
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-05 20:19:01 +0100

    Stage "Hangman". Task 2/2 finished.

--- Hangman/task/src/main/kotlin/jetbrains/kotlin/course/hangman/Main.kt
+++ Hangman/task/src/main/kotlin/jetbrains/kotlin/course/hangman/Main.kt
@@ -1,5 +1,19 @@
 package jetbrains.kotlin.course.hangman
 
+// = TODO("Not yet implemented")
+fun generateNewUserWord(secret: String, guess: Char, currentUserWord: String): String {
+    val newUserWord = currentUserWord.split(" ").toMutableList()
+    for (index in secret.indices) {
+        if (secret[index] == guess) {
+            newUserWord[index] = guess.toString()
+        }
+    }
+    return newUserWord.joinToString(" ")
+}
+
+fun isComplete(secret: String, currentUserWord: String): Boolean =
+    secret == currentUserWord
+
 fun main() {
     // Write your code here
 }
