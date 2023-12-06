b09187a88ef106ecf5d150ffa575f62fd16b989a
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-06 18:46:45 +0100

    Stage "Hangman" is finished.

--- Hangman/task/src/main/kotlin/jetbrains/kotlin/course/hangman/Main.kt
+++ Hangman/task/src/main/kotlin/jetbrains/kotlin/course/hangman/Main.kt
@@ -1,19 +1,93 @@
 package jetbrains.kotlin.course.hangman
 
 // = TODO("Not yet implemented")
+
+fun gameRules() {
+    println(
+        """
+Welcome to the game!
+
+In this game, you need to guess the word chosen by the computer.
+The hidden word will appear as a sequence of underscore characters, one per letter.
+You have $maxAttemptsCount attempts to guess the word.
+All words are English words, consisting of $wordLength letters.
+At each attempt, you should enter one letter; 
+if it is present in the hidden word, all matches will be displayed.
+
+For example, if the word CAT was chosen by the computer, _ _ _ will be displayed first,
+since the word has 3 letters.
+If you enter the letter A, you will see _ A _, and so on.
+
+Good luck with the game!
+        """.trimIndent()
+    )
+}
+
 fun generateNewUserWord(secret: String, guess: Char, currentUserWord: String): String {
-    val newUserWord = currentUserWord.split(" ").toMutableList()
+    val generatedUserWord = currentUserWord.split(" ").toMutableList()
     for (index in secret.indices) {
         if (secret[index] == guess) {
-            newUserWord[index] = guess.toString()
+            generatedUserWord[index] = guess.toString()
         }
     }
-    return newUserWord.joinToString(" ")
+    val newUserWord = generatedUserWord.joinToString(" ")
+    if (isComplete(secret, newUserWord)) {
+        println("Great! This letter is in the word! The current word is: $newUserWord")
+    } else {
+        println("Sorry, the secret does not contain the symbol: $guess. The current word is: $currentUserWord")
+    }
+    return newUserWord
 }
 
 fun isComplete(secret: String, currentUserWord: String): Boolean =
-    secret == currentUserWord
+    secret == currentUserWord.replace(" ", "")
+
+fun generateSecret(): String = words.random()
+
+fun isCorrectInput(userInput: String): Boolean  {
+    if (userInput.length != 1) {
+        println("The length of your guess should be 1! Try again!")
+        return false
+    }
+    if (!userInput.first().isLetter()) {
+        println("You should input only English letters! Try again!")
+        return false
+    }
+    return true
+}
+
+fun safeUserInput(): Char {
+    var userInput = ""
+    for (attempts in 0 .. maxAttemptsCount) {
+        println("Please input your guess.")
+        userInput = safeReadLine().uppercase()
+        if (isCorrectInput(userInput)) {
+            break
+        }
+    }
+    return userInput.first()
+}
+
+fun gamePlay(secretWord: String) {
+
+    var currentUserWord = List(wordLength) {"_"}.joinToString(" ")
+    for (attempts in 0 .. maxAttemptsCount) {
+        val userInput = safeUserInput()
+        currentUserWord = generateNewUserWord(secretWord, userInput, currentUserWord)
+    }
+    if (isComplete(secretWord,currentUserWord)) {
+        println("Congratulations! You guessed it!")
+    } else {
+        println("Sorry, you lost! My word is $secretWord")
+    }
+
+}
 
 fun main() {
-    // Write your code here
+    val secretWord = generateSecret()
+
+    gameRules()
+
+    gamePlay(secretWord)
+
 }
