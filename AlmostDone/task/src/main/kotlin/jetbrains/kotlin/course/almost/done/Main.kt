a0d1d57976979a0a276f6ef0ef56717ce1a8c41c
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-06 19:09:12 +0100

    Stage "AlmostDone": Task 3/8 finished.

--- AlmostDone/task/src/main/kotlin/jetbrains/kotlin/course/almost/done/Main.kt
+++ AlmostDone/task/src/main/kotlin/jetbrains/kotlin/course/almost/done/Main.kt
@@ -1,5 +1,19 @@
 package jetbrains.kotlin.course.almost.done
 
+fun trimPicture(picture: String): String = picture.trimIndent()
+
+fun applyFilter(trimmedPictures: String, filterName: String): String {
+   return when (filterName) {
+       "borders" -> applyBordersFilter(trimmedPictures)
+       "squared" -> applySquaredFilter(trimmedPictures)
+       else -> error("Unexpected number")
+   }
+}
+
+fun applyBordersFilter(trimmedPictures: String): String = TODO("Not yet implemented")
+
+fun applySquaredFilter(trimmedPictures: String): String = TODO("Not yet implemented")
+
 fun main() {
     // Write your solution in this file
 }
