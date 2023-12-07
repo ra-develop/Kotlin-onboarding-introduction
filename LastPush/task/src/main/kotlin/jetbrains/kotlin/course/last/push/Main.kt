b4c96e7065cfb0ecee841af12afd579afef4f568
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-07 17:03:49 +0100

    Stage "Last Push": Task 2/5 is finished.

--- LastPush/task/src/main/kotlin/jetbrains/kotlin/course/last/push/Main.kt
+++ LastPush/task/src/main/kotlin/jetbrains/kotlin/course/last/push/Main.kt
@@ -1,5 +1,14 @@
 package jetbrains.kotlin.course.last.push
 
+fun fillPatternRow(patternRow: String, patternWidth: Int): String {
+    if (patternRow.length > patternWidth) {
+        error("patternRow.length should be < patternWidth")
+    }
+    return patternRow.padEnd(patternWidth, separator)
+}
+
+fun getPatternHeight(pattern: String): Int = pattern.lines().size
+
 fun main() {
     // Write your code here
 }
--- LastPush/task/test/MainClass.kt
+++
@@ -1,9 +0,0 @@
-import org.jetbrains.academy.test.system.core.models.classes.TestClass
-
-internal val mainClass = TestClass(
-    classPackage = "jetbrains.kotlin.course.last.push",
-    customMethods = listOf(
-        fillPatternRowMethod,
-        getPatternHeightMethod,
-    )
-)
