0bcbcbc3c5c8295fc95a8741392b66c7f3eafafb
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-07 21:24:12 +0100

    Stage "Last Push": Task 3/5 is finished.

--- LastPush/task/src/main/kotlin/jetbrains/kotlin/course/last/push/Main.kt
+++ LastPush/task/src/main/kotlin/jetbrains/kotlin/course/last/push/Main.kt
@@ -7,7 +7,45 @@ fun fillPatternRow(patternRow: String, patternWidth: Int): String {
     return patternRow.padEnd(patternWidth, separator)
 }
 
+fun fillPattern(pattern: String): String {
+    val patternLines = pattern.lines()
+    val patternWidth = getPatternWidth(pattern)
+    var filledPattern = ""
+    patternLines.forEachIndexed { index, line ->
+        filledPattern += fillPatternRow(line, patternWidth) + newLineSymbol
+    }
+    return filledPattern.removeSuffix(newLineSymbol)
+}
 fun getPatternHeight(pattern: String): Int = pattern.lines().size
+
+fun canvasGenerator(pattern: String, width: Int, height: Int): String {
+    val filledPattern = fillPattern(pattern)
+    var canvas = repeatHorizontally(filledPattern, width)
+    for (i in 2 .. height) {
+        canvas += repeatHorizontally(dropTopFromLine(filledPattern), width)
+    }
+    return canvas
+}
+
+fun dropTopFromLine(pattern: String): String {
+    val patternLines = pattern.lines()
+    if (patternLines.size == 1) {
+        return pattern
+    }
+    return patternLines.drop(1).joinToString(newLineSymbol).removeSuffix(newLineSymbol)
+}
+
+fun repeatHorizontally(pattern: String, repeatNumber: Int): String {
+    val lines = pattern.lines()
+    val sb = StringBuilder()
+    for (line in lines) {
+        sb.append(newLineSymbol)
+        for (i in 1 .. repeatNumber) {
+            sb.append(line)
+        }
+    }
+    return sb.toString()
+}
 
 fun main() {
     // Write your code here
